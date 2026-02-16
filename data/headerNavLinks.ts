4b1b57cf86043ea4363231fa8238a8e4b3b77771
jdecorte-be <jdecorte@proton.me>
2026-02-16 14:40:46 +0100

    fix nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/work', title: 'Work' },
+  // { href: '/work', title: 'Work' },
   { href: '/writeups', title: 'Write up' },
-  { href: '/terminal', title: '/bin/bash' },
+  // { href: '/terminal', title: '/bin/bash' },
 ]
 
 export default headerNavLinks
