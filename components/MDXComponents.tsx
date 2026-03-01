dee8ed1f6d428a1dbe080fbfb7aeefc11955d159
jdecorte-be <jdecorte@proton.me>
2026-03-01 09:40:45 +0100

    Add animate-ui code block and copy button components

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,5 +1,6 @@
 import TOCInline from "pliny/ui/TOCInline";
 import Pre from "pliny/ui/Pre";
+// import Pre from "pliny/ui/Pre"
 import type { MDXComponents } from "mdx/types";
 import Image from "./Image";
 import CustomLink from "./Link";
--- components/animate-ui/primitives/effects/highlight.tsx
+++ components/animate-ui/primitives/effects/highlight.tsx
@@ -423,11 +423,8 @@ function HighlightItem<T extends React.ElementType>({
   const isDisabled = disabled === undefined ? contextDisabled : disabled;
   const itemTransition = transition ?? contextTransition;
 
-  const localRef = React.useRef<HTMLDivElement | null>(null) as React.MutableRefObject<HTMLDivElement | null>;
-  React.useImperativeHandle(
-    typeof ref !== 'string' ? ref : null,
-    () => localRef.current as HTMLDivElement
-  );
+  const localRef = React.useRef<HTMLDivElement>(null);
+  React.useImperativeHandle(ref, () => localRef.current as HTMLDivElement);
 
   const refCallback = React.useCallback((node: HTMLElement | null) => {
     localRef.current = node as HTMLDivElement;
