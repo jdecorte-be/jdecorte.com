16cac2a54b26553642b431f22ca62879d1899e29
jdecorte-be <jdecorte@proton.me>
2026-04-04 21:51:34 -0400

    Add sharp and plaiceholder, document priority image usage

--- components/core/Image.tsx
+++ components/core/Image.tsx
@@ -3,3 +3,6 @@ import NextImage, { type ImageProps } from "next/image";
 const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />;
 
 export default Image;
+
+// For above-the-fold images, use:
+// <Image priority sizes="(max-width: 768px) 100vw, 50vw" ... />
